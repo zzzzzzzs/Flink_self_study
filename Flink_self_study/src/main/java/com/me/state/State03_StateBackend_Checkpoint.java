3405e10bb6cba8e4f8a93531cbfac42df860c0d1
zzzzzzzs <1443539042@qq.com>
2021-05-08 23:49:36 +0800

    update

--- Flink_self_study/src/main/java/com/me/state/State03_StateBackend_Checkpoint.java
+++ Flink_self_study/src/main/java/com/me/state/State03_StateBackend_Checkpoint.java
@@ -21,25 +21,32 @@ TODO 每传入一条数据，有状态的算子任务都会读取和更新状态
 
 * */
 
-public class State03_StateBackend {
+public class State03_StateBackend_Checkpoint {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        // 1. 状态后端配置
+        // 1. 状态后端配置 StateBackend
         env.setStateBackend(new MemoryStateBackend());
         env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/flink/checkpoints/fs"));
         env.setStateBackend(new RocksDBStateBackend("hdfs://hadoop102:8020/flink/checkpoints/rocksdb"));
 
-        // 2. 检查点配置
+        // TODO 检查点配置 Checkpointing，默认是500ms ，配置检查点也是消耗性能的
         env.enableCheckpointing(300);
 
-        // 高级选项
+        // 2. 高级选项
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         env.getCheckpointConfig().setCheckpointTimeout(60000L);
+        // TODO 有可能前一个checkpoint没有保存完，下一个checkpoint又触发了，同时执行的checkpoint有多少个
         env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+        // TODO 做checkpoint的时候有可能占用一些时间保存，比如隔300ms保存。2个barrier有一个暂歇的时间。
+        //  100ms指的是此时checkpoint保存结束到下一次checkpoint触发开始这个之间的时间不能小于100ms。
+        //  也就是说此时checkpoint保存了250ms，但是下一个barrier来了，但是不能立刻做checkpoint，需要有100ms的间隙。
+        //  这个时间需要处理数据。这个配置会覆盖上面的配置，只有能有1个做checkpoint，只能一个结束，等一段时间才可以做checkpoint。
+        //  如果满足了还是按照原来的配置进行checkpoint。
         env.getCheckpointConfig().setMinPauseBetweenCheckpoints(100L);
         env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
+        // 容忍checkpoint失败的个数
         env.getCheckpointConfig().setTolerableCheckpointFailureNumber(0);
 
         // 3. 重启策略配置

9388d6dea000d590b9c6e004864b7768e3ee7171
zzzzzzzs <1443539042@qq.com>
2021-09-03 19:53:32 +0800

    Update transform_12_process2.java

--- Flink_self_study/src/main/java/com/me/transform/transform_12_process2.java
+++ Flink_self_study/src/main/java/com/me/transform/transform_12_process2.java
@@ -19,22 +19,26 @@ public class transform_12_process2 {
         env.setParallelism(1);
         // 不写分区的话就默认是电脑的核数
         env
-                .fromElements(1, 2, 3, 4)
-                .keyBy(r->r.intValue())
+                .fromElements(1, 2, 3, 4, 1)
+                // TODO 由于是分布式的， keyBy会将数据逻辑分区，不同的数据对应不同的 process算子，此时状态也是不一样的。如果想让状态一样可以将keyBy中的参数设置成一样的
+                .keyBy(r -> 1)
                 // TODO rich函数都可以当参数传入, 在keyBy之前的流上使用
                 .process(new ProcessFunction<Integer, Integer>() {
                     // TODO 这里在 process 中使用了状态变量就必须有 keyBy 操作。如果没有keyBy在有限流中会报错
-                    private ValueState<String> state;
+                    private ValueState<Integer> state;
+
                     @Override
                     public void open(Configuration parameters) throws Exception {
                         super.open(parameters);
                         System.out.println("------");
-                        state = getRuntimeContext().getState(new ValueStateDescriptor<>("state", String.class));
+                        state = getRuntimeContext().getState(new ValueStateDescriptor<>("state", Integer.class));
                     }
+
                     // TODO 数据来一条处理一条
                     @Override
                     public void processElement(Integer value, Context ctx, Collector<Integer> out) throws Exception {
-                        state.update("a");
+                        state.update(state.value() != null ? state.value() + 1 : 0);
+                        System.out.println("此时计数器为：" + state.value());
                         out.collect(value);
                     }
                 })

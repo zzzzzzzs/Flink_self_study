bee0d857908c90bbc1889898a27e6ffd0234af3e
zzzzzzzs <1443539042@qq.com>
2022-03-29 09:18:30 +0800

    update

--- Flink_self_study/src/main/java/com/me/tuning/window_max_min_temp.java
+++ Flink_self_study/src/main/java/com/me/tuning/window_max_min_temp.java
@@ -16,79 +16,95 @@ import com.me.source.SensorSource;
 
 // TODO 求窗口的最大温度值和最小温度值
 public class window_max_min_temp {
-    public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+  public static void main(String[] args) throws Exception {
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        env
-                .addSource(new SensorSource())
-                .filter(r -> r.id.equals("sensor_1"))
-                .keyBy(r -> r.id)
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-                .aggregate(new Agg(), new WindowResult())
-                .print();
+    env.addSource(new SensorSource())
+        .filter(r -> r.id.equals("sensor_1"))
+        .keyBy(r -> r.id)
+        .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+        .aggregate(new Agg(), new WindowResult())
+        .print();
 
-        env.execute();
+    env.execute();
+  }
+
+  public static class Agg
+      implements AggregateFunction<SensorReading, Tuple2<Double, Double>, Tuple2<Double, Double>> {
+    @Override
+    public Tuple2<Double, Double> createAccumulator() {
+      // 最大值的初始值是负无穷大
+      // 最小值的初始值是无穷大
+      return Tuple2.of(-Double.MAX_VALUE, Double.MAX_VALUE);
     }
 
-    public static class Agg implements AggregateFunction<SensorReading, Tuple2<Double, Double>, Tuple2<Double, Double>> {
-        @Override
-        public Tuple2<Double, Double> createAccumulator() {
-            // 最大值的初始值是负无穷大
-            // 最小值的初始值是无穷大
-            return Tuple2.of(-Double.MAX_VALUE, Double.MAX_VALUE);
-        }
-
-        @Override
-        public Tuple2<Double, Double> add(SensorReading value, Tuple2<Double, Double> accumulator) {
-            return Tuple2.of(Math.max(value.temperature, accumulator.f0), Math.min(value.temperature, accumulator.f1));
-        }
-
-        @Override
-        public Tuple2<Double, Double> getResult(Tuple2<Double, Double> accumulator) {
-            return accumulator;
-        }
-
-        @Override
-        public Tuple2<Double, Double> merge(Tuple2<Double, Double> a, Tuple2<Double, Double> b) {
-            return null;
-        }
+    @Override
+    public Tuple2<Double, Double> add(SensorReading value, Tuple2<Double, Double> accumulator) {
+      return Tuple2.of(
+          Math.max(value.temperature, accumulator.f0), Math.min(value.temperature, accumulator.f1));
     }
 
-    public static class WindowResult extends ProcessWindowFunction<Tuple2<Double, Double>, MaxMinTempPerWindow, String, TimeWindow> {
-        @Override
-        public void process(String s, Context context, Iterable<Tuple2<Double, Double>> iterable, Collector<MaxMinTempPerWindow> collector) throws Exception {
-            Tuple2<Double, Double> tuple2 = iterable.iterator().next();
-            collector.collect(new MaxMinTempPerWindow(s, tuple2.f0, tuple2.f1, context.window().getStart(), context.window().getEnd()));
-        }
+    @Override
+    public Tuple2<Double, Double> getResult(Tuple2<Double, Double> accumulator) {
+      return accumulator;
     }
 
-    public static class MaxMinTempPerWindow {
-        public String id;
-        public Double maxTemp;
-        public Double minTemp;
-        public Long windowStart;
-        public Long windowEnd;
-
-        public MaxMinTempPerWindow() {
-        }
-
-        public MaxMinTempPerWindow(String id, Double maxTemp, Double minTemp, Long windowStart, Long windowEnd) {
-            this.id = id;
-            this.maxTemp = maxTemp;
-            this.minTemp = minTemp;
-            this.windowStart = windowStart;
-            this.windowEnd = windowEnd;
-        }
-
-        @Override
-        public String toString() {
-            return "MaxMinTempPerWindow{" +
-                    "id='" + id + '\'' +
-                    ", maxTemp=" + maxTemp +
-                    ", minTemp=" + minTemp +
-                    ", windowStart=" + new Timestamp(windowStart) +
-                    ", windowEnd=" + new Timestamp(windowEnd) +
-                    '}';
-        }
+    @Override
+    public Tuple2<Double, Double> merge(Tuple2<Double, Double> a, Tuple2<Double, Double> b) {
+      return null;
     }
+  }
+
+  public static class WindowResult
+      extends ProcessWindowFunction<
+          Tuple2<Double, Double>, MaxMinTempPerWindow, String, TimeWindow> {
+    @Override
+    public void process(
+        String s,
+        Context context,
+        Iterable<Tuple2<Double, Double>> iterable,
+        Collector<MaxMinTempPerWindow> collector)
+        throws Exception {
+      Tuple2<Double, Double> tuple2 = iterable.iterator().next();
+      collector.collect(
+          new MaxMinTempPerWindow(
+              s, tuple2.f0, tuple2.f1, context.window().getStart(), context.window().getEnd()));
+    }
+  }
+
+  public static class MaxMinTempPerWindow {
+    public String id;
+    public Double maxTemp;
+    public Double minTemp;
+    public Long windowStart;
+    public Long windowEnd;
+
+    public MaxMinTempPerWindow() {}
+
+    public MaxMinTempPerWindow(
+        String id, Double maxTemp, Double minTemp, Long windowStart, Long windowEnd) {
+      this.id = id;
+      this.maxTemp = maxTemp;
+      this.minTemp = minTemp;
+      this.windowStart = windowStart;
+      this.windowEnd = windowEnd;
+    }
+
+    @Override
+    public String toString() {
+      return "MaxMinTempPerWindow{"
+          + "id='"
+          + id
+          + '\''
+          + ", maxTemp="
+          + maxTemp
+          + ", minTemp="
+          + minTemp
+          + ", windowStart="
+          + new Timestamp(windowStart)
+          + ", windowEnd="
+          + new Timestamp(windowEnd)
+          + '}';
+    }
+  }
 }

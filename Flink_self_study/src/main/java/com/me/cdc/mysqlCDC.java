748e364139ab20d5bfef8474df6a937b0e6bbff8
zzzzzzzs <1443539042@qq.com>
2022-03-31 11:21:59 +0800

    update

--- Flink_self_study/src/main/java/com/me/cdc/mysqlCDC.java
+++ Flink_self_study/src/main/java/com/me/cdc/mysqlCDC.java
@@ -1,37 +1,39 @@
 package com.me.cdc;
 
-import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
-import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
-import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
-
 
 /**
  * @author zs
  * @date 2021/11/8
  */
 public class mysqlCDC {
-    public static void main(String[] args) throws Exception {
-        SourceFunction<String> sourceFunction = MySQLSource.<String>builder()
-                .hostname("localhost")
-                .port(3306)
-                .username("root")
-                .password("111")
-                .databaseList("mydb")
-                // 指定某个表的变化
-                .tableList("mydb.information_schema.columns")
-                .startupOptions(StartupOptions.initial())
-                .deserializer(new StringDebeziumDeserializationSchema())
-                .build();
+  public static void main(String[] args) throws Exception {
+    MySqlSource<String> mySqlSource =
+        MySqlSource.<String>builder()
+            .hostname("localhost")
+            .port(3306)
+            .databaseList("mydb") // set captured database
+            .tableList("mydb.aaa") // set captured table
+            .username("root")
+            .password("111")
+            .deserializer(
+                new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
+            .build();
 
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        env
-                .addSource(sourceFunction)
-                .print(">>>>>"); // use parallelism 1 for sink to keep message ordering
+    // enable checkpoint
+    //    env.enableCheckpointing(3000);
 
-        env.execute();
-    }
+    env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+        // set 4 parallel source tasks
+        .setParallelism(1)
+        .print()
+        .setParallelism(1); // use parallelism 1 for sink to keep message ordering
+
+    env.execute("Print MySQL Snapshot + Binlog");
+  }
 }
